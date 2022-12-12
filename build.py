0c0fa508da9419d3a9bb033e44fdc4de736cab54
danielnaumau <sadfreddy@gmail.com>
2022-12-12 09:37:33 +0100

    add build of proto files

--- build.py
+++ build.py
@@ -1,6 +1,4 @@
-import pathlib
 import os
-from subprocess import check_call
 from setuptools.command.install import install
 
 
@@ -8,21 +6,10 @@ class CustomBuild(install):
 
     @staticmethod
     def __generate_proto():
-        print("helo")
-        proto_interface_dir = "./proto"
-        generated_src_dir = "traveltimepy/proto/"
-        out_folder = "traveltimepy"
-        if not os.path.exists(generated_src_dir):
-            os.mkdir(generated_src_dir)
-        #proto_it = pathlib.Path().glob(proto_interface_dir + "/**/*")
-        #proto_path = "generated=" + proto_interface_dir
-        #protos = [str(proto) for proto in proto_it if proto.is_file()]
-        #check_call(["protoc"] + protos + ["--python_out", out_folder, "--proto_path", proto_path])
-        #protoc -I=/proto --python_out=/traveltimepy/dto/proto /proto/*
+        os.system('protoc -I=./proto --python_out=./ ./proto/*')
 
     def run(self):
-        print("Custom install start")
         super().run()
-        #self.__generate_proto()
-        print("Custom install done")
-
+        print('File generation started')
+        self.__generate_proto()
+        print('File generation done')
