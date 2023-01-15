735d55a29e5eec106919bcd6f45b65e9c3bbd105
danielnaumau <sadfreddy@gmail.com>
2023-01-15 16:27:27 +0100

    add benchmarks

--- benchmarks/time_filter.py
+++ benchmarks/time_filter.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 import random
 from datetime import datetime
@@ -20,15 +19,15 @@ def generate_locations(lat: float, lng: float, radius: float, name: str, amount:
     ]
 
 
-async def send():
-    sdk = TravelTimeSdk("APP_ID", "API_KEY")
-    locations = generate_locations(51.507609, -0.128315, 0.05, 'Location', 50)
+def generate_matrix(size: int):
+    sdk = TravelTimeSdk('APP_ID', 'API_KEY')
+    locations = generate_locations(51.507609, -0.128315, 0.05, 'Location', size)
     location_ids = [location.id for location in locations]
     searches = [
         (location_id, list(filter(lambda cur_id: cur_id != location_id, location_ids)))
         for location_id in location_ids
     ]
-    return await sdk.time_filter_async(
+    return sdk.time_filter(
         locations=locations,
         searches=dict(searches),
         transportation=Driving(),
@@ -38,7 +37,16 @@ async def send():
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    response = asyncio.run(send())
-    print(response.results)
+    response = generate_matrix(50)
     request_time = time.perf_counter() - start
-    print("Request completed in {0:.0f}s".format(request_time))
+    print("Request completed in {0:.0f}s for matrix 50 * 50".format(request_time))
+
+    start = time.perf_counter()
+    response2 = generate_matrix(100)
+    request_time = time.perf_counter() - start
+    print("Request completed in {0:.0f}s for matrix 100 * 100".format(request_time))
+
+    start = time.perf_counter()
+    response3 = generate_matrix(300)
+    request_time = time.perf_counter() - start
+    print("Request completed in {0:.0f}s for matrix 300 * 300".format(request_time))
