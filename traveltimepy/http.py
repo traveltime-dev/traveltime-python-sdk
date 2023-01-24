d4554da122646e866e2e2cde6d32744fa7f0f45c
danielnaumau <sadfreddy@gmail.com>
2023-01-24 14:59:02 +0100

    remove prints

--- traveltimepy/http.py
+++ traveltimepy/http.py
@@ -74,15 +74,12 @@ def send_get(
     headers: Dict[str, str],
     params: Dict[str, str] = None
 ) -> T:
-    print(params)
     return asyncio.run(send_get_async(response_class, path, headers, params))
 
 
 async def __process_response(response_class: Type[T], response: ClientResponse) -> T:
     text = await response.text()
     if response.status != 200:
-        print(text)
-
         parsed = parse_raw_as(ResponseError, text)
         msg = 'Travel Time API request failed \n{}\nError code: {}\nAdditional info: {}\n<{}>\n'.format(
             parsed.description,
