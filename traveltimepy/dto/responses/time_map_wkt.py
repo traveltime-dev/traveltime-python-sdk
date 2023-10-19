0c47d8d4ac408646d286366b3ba837dcbedd757e
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-10-19 16:29:20 +0300

    Implemented solution with own WKT lib

--- traveltimepy/dto/responses/time_map_wkt.py
+++ traveltimepy/dto/responses/time_map_wkt.py
@@ -1,43 +1,25 @@
 from typing import List, Union, Dict, Any, TypeVar, Iterator, Optional
 from pydantic import BaseModel, Field, validator
 from pydantic.generics import GenericModel
-from shapely import wkt
 from shapely.errors import WKTReadingError
+
+from simple_wkt import WKTObject, parse_wkt
 
 Props = TypeVar("Props", bound=Union[Dict[str, Any], BaseModel])
 
 
-class WKT(BaseModel):
-    value: str = Field(..., description="WKT string representation of the geometry.")
-
-    @validator("value", pre=True)
-    def validate_wkt(cls, value: str) -> str:
-        try:
-            wkt.loads(value)
-            return value
-        except WKTReadingError:
-            raise ValueError("Invalid WKT string")
-
-
 class TimeMapWKTResult(GenericModel):
     search_id: str
-    shape: WKT
+    shape: WKTObject
     properties: Optional[Props] = Field(None)
 
     @validator("shape", pre=True)
-    def transform_shape(cls, shape: str) -> Dict[str, str]:
-        return {"value": shape}
-
-    # Used for getting json format response
-    #
-    # Example:
-    # results_dicts = [result.dict() for result in results]
-    # results_json_str = json.dumps(results_dicts)
-    # print(results_json_str)
-    def dict(self, **kwargs):
-        original_dict = super().dict(**kwargs)
-        original_dict['shape'] = original_dict['shape']['value']
-        return original_dict
+    def transform_shape(cls, shape: str) -> WKTObject:
+        try:
+            # Assuming WKTObject's constructor can handle a WKT string
+            return parse_wkt(shape)
+        except WKTReadingError:
+            raise ValueError("Invalid WKT string")
 
 
 class WKTResponseCollection(BaseModel):
