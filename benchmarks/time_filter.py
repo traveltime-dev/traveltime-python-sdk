152b3036cfe3d91c1e751e221d21d88e26681210
donatas <donatas@localhost.localdomain>
2025-07-01 13:25:05 +0300

    Close connections in benchmarks

--- benchmarks/time_filter.py
+++ benchmarks/time_filter.py
@@ -10,26 +10,26 @@ from traveltimepy.requests.time_filter import TimeFilterDepartureSearch
 
 
 async def generate_matrix(size: int):
-    async_client = AsyncClient("APP_ID", "API_KEY")
-    locations = generate_locations(51.507609, -0.128315, 0.05, "Location", size)
-    location_ids = [location.id for location in locations]
-    searches = [
-        TimeFilterDepartureSearch(
-            id=location_id,
-            departure_location_id=location_id,
-            arrival_location_ids=list(
-                filter(lambda cur_id: cur_id != location_id, location_ids)
-            ),
-            departure_time=datetime.now(),
-            transportation=Driving(),
-            travel_time=3600,
-            properties=[Property.TRAVEL_TIME],
+    async with AsyncClient("APP_ID", "API_KEY") as async_client:
+        locations = generate_locations(51.507609, -0.128315, 0.05, "Location", size)
+        location_ids = [location.id for location in locations]
+        searches = [
+            TimeFilterDepartureSearch(
+                id=location_id,
+                departure_location_id=location_id,
+                arrival_location_ids=list(
+                    filter(lambda cur_id: cur_id != location_id, location_ids)
+                ),
+                departure_time=datetime.now(),
+                transportation=Driving(),
+                travel_time=3600,
+                properties=[Property.TRAVEL_TIME],
+            )
+            for location_id in location_ids
+        ]
+        return await async_client.time_filter(
+            locations=locations, departure_searches=searches, arrival_searches=[]
         )
-        for location_id in location_ids
-    ]
-    return await async_client.time_filter(
-        locations=locations, departure_searches=searches, arrival_searches=[]
-    )
 
 
 if __name__ == "__main__":
